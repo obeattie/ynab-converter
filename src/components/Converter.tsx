70008a67f555d0a6533589cf8921d62af1a1831f
Oliver Beattie <oliver@obeattie.com>
2025-02-25 21:20:47 +0100

    Minor changes to Converter

--- src/components/Converter.tsx
+++ src/components/Converter.tsx
@@ -4,15 +4,22 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import type { Transaction } from "../transaction";
 import { ynabCSV } from "../writer";
 
+export type ConverterF = (input: ArrayBufferLike) => Promise<Transaction[]>;
+
 type ConvertWidgetProps = {
 	name: string;
-	f: (input: ArrayBuffer) => Promise<Transaction[]>;
+	f: ConverterF;
+};
+
+type Output = {
+	blob: Blob;
+	downloadTriggered: boolean;
 };
 
 export default function Converter(converter: ConvertWidgetProps) {
 	const uploadField = useRef<HTMLInputElement>(null);
 	const outputLink = useRef<HTMLAnchorElement>(null);
-	const [output, setOutput] = useState<Blob | null>(null);
+	const [output, setOutput] = useState<Output | null>(null);
 	const [isDragging, toggleDragging] = useToggle();
 
 	const onChange = useCallback(async () => {
@@ -26,14 +33,17 @@ export default function Converter(converter: ConvertWidgetProps) {
 		);
 		const txs = (await Promise.all(txPromises)).flat();
 		const output = ynabCSV(txs);
-		setOutput(new Blob([output], { type: "text/csv" }));
+		setOutput({
+			blob: new Blob([output], { type: "text/csv" }),
+			downloadTriggered: false,
+		});
 	}, [converter]);
 
-	// If there's a file ready to download, click the link to make it download and then clear it
+	// If there's a file ready to download and it hasn't been auto-downloaded, click the link
 	useEffect(() => {
-		if (output && outputLink.current) {
+		if (output && !output.downloadTriggered && outputLink.current) {
 			outputLink.current.click();
-			setOutput(null);
+			setOutput({ ...output, downloadTriggered: true });
 		}
 	}, [output]);
 
@@ -53,16 +63,16 @@ export default function Converter(converter: ConvertWidgetProps) {
 				onDrop={() => toggleDragging(false)}
 				ref={uploadField}
 				className="absolute inset-0 opacity-0"
+				data-testid="input-upload"
 			/>
 			<p className="font-bold w-full my-auto">{converter.name}</p>
 			{output && (
 				<a
 					ref={outputLink}
-					href={output ? URL.createObjectURL(output) : "#"}
-					download={
-						output && `transactions-${converter.name.toLowerCase()}.csv`
-					}
+					href={output ? URL.createObjectURL(output.blob) : "#"}
+					download={`transactions-${converter.name.toLowerCase()}.csv`}
 					className="hidden"
+					data-testid="output-download"
 				>
 					Download output
 				</a>
